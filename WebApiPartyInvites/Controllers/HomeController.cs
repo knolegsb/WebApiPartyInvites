4faac1aeaafb25394c98e32b700756f6e974e358
knolegsb <seon2you@hotmail.com>
2015-11-10 09:18:01 -0800

    added untracked files

---
+++ WebApiPartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApiPartyInvites.Models;
+
+namespace WebApiPartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Default()
+        {
+            return View();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Rsvp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Rsvp(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(response);
+                return View("Thanks", response);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [ChildActionOnly]
+        public ActionResult Attendees()
+        {
+            return View(Repository.Responses.Where(x => x.WillAttend == true));
+        }
+    }
+}
